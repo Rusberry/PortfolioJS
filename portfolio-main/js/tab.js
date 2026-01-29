8920fed7c804e184dbce7e90003aef174d14df48
Rusberry <44698561+Rusberry@users.noreply.github.com>
2026-01-29 09:48:01 +0300

    First

---
+++ portfolio-main/js/tab.js
@@ -0,0 +1,43 @@
+const tabButtons = document.querySelectorAll('.design-list__item');
+const tabDescriptions = document.querySelectorAll('.design__descr');
+const tabImages = document.querySelectorAll('.design-images')
+
+const changeContent = (array, value)=>{
+    array.forEach((elem)=>{
+            if (elem.dataset.tabsField === value){
+                elem.classList.toggle('hidden')
+            } else {
+                elem.classList.toggle('hidden')
+            }
+        })
+}
+
+tabButtons.forEach((tabButton)=>{
+    tabButton.addEventListener('click', (event)=>{
+        const dataValue = tabButton.dataset.tabsHandler
+        changeContent(tabDescriptions, dataValue)
+        changeContent(tabImages, dataValue)
+        // tabDescriptions.forEach((descr)=>{
+        //     if (descr.dataset.tabsField === dataValue){
+        //         descr.classList.toggle('hidden')
+        //     } else {
+        //         descr.classList.toggle('hidden')
+        //     }
+        // })
+        // tabImages.forEach((img, indexDescr)=>{
+        //     if (img.dataset.tabsField === dataValue){
+        //         img.classList.toggle('hidden')
+        //     } else {
+        //         img.classList.toggle('hidden')
+        //     }
+        // })
+        tabButtons.forEach((btn)=>{
+            if(btn === event.target){
+                document.title = btn.textContent;
+                btn.classList.toggle('design-list__item_active')
+            } else {
+                btn.classList.toggle('design-list__item_active')
+            }
+        })
+    })
+});
